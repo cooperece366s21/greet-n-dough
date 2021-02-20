9422168acc9053fed899314795a3b2b3a0180093
doanhub <doan@cooper.edu>
2021-02-20 17:15:50 -0500

    Create Image.java
    Can now choose file path and move a copy of image to data directory

---
+++ Greet-Dough/src/main/java/Image.java
@@ -0,0 +1,69 @@
+// Import the Scanner class
+import java.util.Scanner;
+
+// Move file libraries
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import java.io.Serializable;
+
+public class Image implements Serializable {
+        // Prompt user to input an image path
+            //input path for terminal (first phase)
+        // Copy the image from the path to the data directory
+            // os.system("mv 'picture.png' "")
+
+
+        // Get image ID
+            // Post class
+        // Get User ID
+            // have to be logged in
+            // return from the get user id function
+            // associate the image id with the user id
+        // Get Post ID
+            //once post is created, exist a post ID
+            //associate the image id with the post ID
+
+        // 8 place number, 24 digits (hex on id)
+        // user id how many places,
+        // img id is how many places
+        // post id is how many places
+
+        Image() {}
+            public static String getImage() {
+                Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+                System.out.println("Enter filepath");
+
+                String imagePath = myObj.nextLine();  // Read user input
+                String extension = "";
+                int i = imagePath.lastIndexOf('.');
+                if (i >= 0) { extension = imagePath.substring(i+1); }
+
+                String validTypes = "jpg png jpeg";
+                if (!(validTypes.contains(extension))){
+                    System.out.println("Invalid Input, Please Input a Valid Image!");
+                    imagePath = getImage();
+                }
+                return imagePath;
+            }
+
+            public static void moveImage() {
+                FileSystem fileSys = FileSystems.getDefault();
+                Path srcPath = fileSys.getPath(getImage());
+                //change this abomination if you are testing it (for now)
+                //NEED TO EDIT LATER
+                Path destPath = fileSys.getPath("c:\\Users\\brian\\OneDrive\\Documents\\Github\\Lee-Ko\\Greet-Dough\\data\\images.png");
+                try {
+                    //COPY image from source to destination folder
+                    Files.copy(srcPath, destPath, StandardCopyOption.REPLACE_EXISTING);
+
+
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+}
