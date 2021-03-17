216dea6c298ce2ae18e90172ecb8f3cf1cfc5442
Thodori Kapouranis <thodorikap@gmail.com>
2021-03-17 19:20:47 -0400

    Set up basic header

--- greet-dough-frontend/src/index.tsx
+++ greet-dough-frontend/src/index.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Header from './components/Header'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Header />,
+    <App />,
   document.getElementById('root')
 );
 
