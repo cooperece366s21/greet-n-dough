c7287d46ebe56e1575356f55ae4cefa0f6714e51
doanhub <doan@cooper.edu>
2021-04-03 22:22:50 -0400

    Updated ImageStore, ImageStoreImpl

--- Greet-Dough-Backend/src/main/java/store/model/ImageStore.java
+++ Greet-Dough-Backend/src/main/java/store/model/ImageStore.java
@@ -16,43 +16,8 @@ public interface ImageStore {
 
     boolean deleteImage( int ID );
 
-    static String uploadImage( Image newImage ) {
+    String uploadImage( Image newImage );
 
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter filepath: ");
-
-        String path = myObj.nextLine();  // Read user input
-        String extension = "";
-        int i = path.lastIndexOf('.');
-        if ( i >= 0 ) {
-            extension = path.substring(i + 1);
-        }
-
-        String validTypes = "jpg png jpeg";
-        if ( !(validTypes.contains(extension)) ) {
-            System.out.println("Invalid Input, Please Input a Valid Image!");
-            path = uploadImage(newImage);
-        }
-        newImage.setPath(path);
-        return path;
-
-    }
-
-    static void moveImage( Image newImage ) {
-
-        FileSystem fileSys = FileSystems.getDefault();
-        Path srcPath = fileSys.getPath(uploadImage(newImage));
-        //change this abomination if you are testing it (for now)
-        //NEED TO EDIT LATER
-        Path destPath = fileSys.getPath("c:\\Users\\brian\\OneDrive\\Documents\\Github\\Lee-Ko\\Greet-Dough\\data\\images.png");
-        try {
-            //COPY image from source to destination folder
-            Files.copy(srcPath, destPath, StandardCopyOption.REPLACE_EXISTING);
-
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-
-    }
+    void moveImage( Image newImage );
 
 }
