9d5ea3d9030c540395f19eda3d7ea5c51e24841f
doanhub <doan@cooper.edu>
2021-04-06 23:03:19 -0400

    Update LikeDao.java

--- Greet-Dough-Backend/src/main/java/store/postgres/LikeDao.java
+++ Greet-Dough-Backend/src/main/java/store/postgres/LikeDao.java
@@ -41,11 +41,11 @@ public interface LikeDao {
 
     @SqlUpdate("CREATE TABLE IF NOT EXISTS like( " +
             "post_id INT " +        "NOT NULL, " +
-            "user_id INT " +        "NOT NULL, " +
+            "user_id INT[] " +        "NOT NULL, " +
             "PRIMARY KEY(user_id), " +
             "CONSTRAINT fk_user " + "FOREIGN KEY(user_id) " +
                 "REFERENCES users(user_id) " + "ON DELETE CASCADE " +
-            "CONSTRAINT fk_user " + "FOREIGN KEY(post_id) " +
+            "CONSTRAINT fk_post " + "FOREIGN KEY(post_id) " +
                 "REFERENCES post(post_id) " +
             ");")
     void createTable();
