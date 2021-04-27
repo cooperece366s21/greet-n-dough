36e464282cbd94db269fd68f779ca7474c748f58
Derek Lee <51767748+DerekLeeCS@users.noreply.github.com>
2021-04-26 20:09:09 -0400

    wrote LikeStorePostgres unit test

--- Greet-Dough-Backend/src/main/java/store/postgres/LikeStorePostgres.java
+++ Greet-Dough-Backend/src/main/java/store/postgres/LikeStorePostgres.java
@@ -1,56 +1,11 @@
 package store.postgres;
 
-import model.User;
-import model.Post;
 import model.Likes;
 import store.model.LikeStore;
-import utility.ResetDao;
 
 import org.jdbi.v3.core.Jdbi;
 
 public class LikeStorePostgres implements LikeStore {
-
-    // For testing purposes
-    public static void main( String[] args ) {
-
-        Jdbi jdbi = GreetDoughJdbi.create("jdbc:postgresql://localhost:4321/greetdough");
-        UserStorePostgres UserStorePostgres = new UserStorePostgres(jdbi);
-        PostStorePostgres PostStorePostgres = new PostStorePostgres(jdbi);
-        LikeStorePostgres LikeStorePostgres = new LikeStorePostgres(jdbi);
-
-        // Used to DROP and CREATE all tables
-        ResetDao.reset(jdbi);
-
-        User newUser = UserStorePostgres.addUser("Felipe");
-        User tempUser = UserStorePostgres.addUser("Jill");
-
-        // Make two posts
-        Post newPost = PostStorePostgres.addPost( "My first post", "first!", newUser.getID() );
-        Post secondPost = PostStorePostgres.addPost( "My second post", "haha very cool!", newUser.getID() );
-
-        // Like one post
-        LikeStorePostgres.addUserLike( newPost.getID(), newUser.getID() );
-        LikeStorePostgres.addUserLike( newPost.getID(), tempUser.getID() );
-
-        // Check who liked the post
-        System.out.println( LikeStorePostgres.getLikes( newPost.getID() ).getUserLikes() );
-        System.out.println( LikeStorePostgres.getLikes( secondPost.getID() ).getUserLikes() );
-
-        // Count how many people liked a post
-        System.out.println( LikeStorePostgres.getLikes( newPost.getID() ).getLikeCount() );
-
-        // A single user unlikes a post
-        LikeStorePostgres.deleteUserLike(newPost.getID(), newUser.getID());
-        System.out.println( LikeStorePostgres.getLikes( newPost.getID() ).getUserLikes() );
-
-        // Check if the user liked a specific post
-        System.out.println( LikeStorePostgres.hasUserLike( newPost.getID(), newUser.getID() ) );
-
-        // Delete post => delete all likes
-        PostStorePostgres.deletePost( newPost.getID() );
-        System.out.println( LikeStorePostgres.getLikes( newPost.getID() ) );
-
-    }
 
     private final Jdbi jdbi;
 
---
+++ Greet-Dough-Backend/src/test/java/store/postgres/LikeStorePostgresTest.java
@@ -0,0 +1,67 @@
+package store.postgres;
+
+import model.Post;
+import model.User;
+import org.jdbi.v3.core.Jdbi;
+import org.junit.jupiter.api.Test;
+import utility.ResetDao;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LikeStorePostgresTest extends LikeStorePostgres {
+
+    private static final Jdbi jdbi = GreetDoughJdbi.create("jdbc:postgresql://localhost:4321/greetdough");
+
+    public LikeStorePostgresTest() {
+        super(jdbi);
+    }
+
+    @Test
+    void test() {
+
+        UserStorePostgres userStorePostgres = new UserStorePostgres(jdbi);
+        PostStorePostgres postStorePostgres = new PostStorePostgres(jdbi);
+        LikeStorePostgres likeStorePostgres = new LikeStorePostgres(jdbi);
+
+        // Used to DROP and CREATE all tables
+        ResetDao.reset(jdbi);
+
+        User newUser = userStorePostgres.addUser("Felipe");
+        User tempUser = userStorePostgres.addUser("Jill");
+
+        // Make two posts
+        Post newPost = postStorePostgres.addPost( "My first post", "first!", newUser.getID() );
+        Post secondPost = postStorePostgres.addPost( "My second post", "haha very cool!", newUser.getID() );
+
+        // Like one post
+        likeStorePostgres.addUserLike( newPost.getID(), newUser.getID() );
+        likeStorePostgres.addUserLike( newPost.getID(), tempUser.getID() );
+        HashSet<Integer> correctLikes = new HashSet<>();
+        correctLikes.add( newUser.getID() );
+        correctLikes.add( tempUser.getID() );
+
+        // Check who liked the post
+        assert ( likeStorePostgres.getLikes( newPost.getID() ).getUserLikes().equals(correctLikes) );
+        assert ( likeStorePostgres.getLikes( secondPost.getID() ).getUserLikes().isEmpty() );
+
+        // Count how many people liked a post
+        assert ( likeStorePostgres.getLikes( newPost.getID() ).getLikeCount() == 2 );
+
+        // A single user unlikes a post
+        likeStorePostgres.deleteUserLike( newPost.getID(), newUser.getID() );
+        correctLikes.remove( newUser.getID() );
+        assert ( likeStorePostgres.getLikes( newPost.getID() ).getUserLikes().equals(correctLikes) );
+
+        // Check if the user liked a specific post
+        assertFalse ( likeStorePostgres.hasUserLike( newPost.getID(), newUser.getID() ) );
+
+        // Delete post
+        //      Should delete cascade all user likes
+        postStorePostgres.deletePost( newPost.getID() );
+        assert ( likeStorePostgres.getLikes( newPost.getID() ).getUserLikes().isEmpty() );
+
+    }
+
+}
