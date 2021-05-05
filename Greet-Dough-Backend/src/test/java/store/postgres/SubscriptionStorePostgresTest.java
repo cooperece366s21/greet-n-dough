feb5ab18ed45c80daac4d0047d404f4b40d8864f
doanhub <doan@cooper.edu>
2021-05-05 19:41:42 -0400

    Update SubscriptionStorePostgresTest.java

--- Greet-Dough-Backend/src/test/java/store/postgres/SubscriptionStorePostgresTest.java
+++ Greet-Dough-Backend/src/test/java/store/postgres/SubscriptionStorePostgresTest.java
@@ -23,7 +23,7 @@ class SubscriptionStorePostgresTest extends SubscriptionStorePostgres {
     void test() {
 
         UserStorePostgres userStorePostgres = new UserStorePostgres(jdbi);
-
+        SubscriptionStorePostgres subscriptionStorePostgres = new SubscriptionStorePostgres(jdbi);
         // Used to DROP and CREATE all tables
         ResetDao.reset(jdbi);
 
@@ -31,8 +31,48 @@ class SubscriptionStorePostgresTest extends SubscriptionStorePostgres {
         User newUser_1 = userStorePostgres.addUser("Ban Dim");
         User newUser_2 = userStorePostgres.addUser("Bill Bluemint");
         User newUser_3 = userStorePostgres.addUser("Vim Kim");
+        User newUser_4 = userStorePostgres.addUser("Poki Vim");
 
+        // Test Subscribing with positive tier values
+        subscriptionStorePostgres.addSubscription( newUser_1.getID(), newUser_2.getID(), 3 );
 
+        // Test retrieving the list of subscribers
+        List<UserTier> newUser_1_subscriptions = subscriptionStorePostgres.getSubscriptions( newUser_1.getID() );
+        assert ( newUser_1_subscriptions.size() == 1 );
+
+        // Test retrieving the empty list of followers
+        assert ( subscriptionStorePostgres.getFollowers( newUser_1.getID() ).isEmpty() );
+
+        // Test retrieving the list of followers
+        List<UserTier> newUser_2_followers = subscriptionStorePostgres.getFollowers( newUser_2.getID() );
+        assert ( newUser_2_followers.size() == 1 );
+
+        // Test Subscribing with free tier (tier 0)
+        subscriptionStorePostgres.addSubscription( newUser_3.getID(), newUser_4.getID() );
+
+        // Test retrieving the list of subscribers
+        List<UserTier> newUser_3_subscriptions = subscriptionStorePostgres.getSubscriptions( newUser_3.getID() );
+        assert ( newUser_3_subscriptions.size() == 1 );
+
+        // Test retrieving the list of followers
+        List<UserTier> newUser_4_followers = subscriptionStorePostgres.getFollowers( newUser_4.getID() );
+        assert ( newUser_4_followers.size() == 1 );
+
+        // Test subscribing back to a follower
+        subscriptionStorePostgres.addSubscription( newUser_4.getID(), newUser_3.getID() );
+        List<UserTier> newUser_3_followers = subscriptionStorePostgres.getFollowers( newUser_3.getID() );
+        assert ( newUser_3_followers.size() == 1 );
+
+        List<UserTier> newUser_4_subscriptions = subscriptionStorePostgres.getSubscriptions( newUser_4.getID() );
+        assert ( newUser_4_subscriptions.size() == 1 );
+
+        // Test deleting the subscription
+        //      Should delete cascade the subscribers and followers
+        subscriptionStorePostgres.deleteSubscription( newUser_3.getID(), newUser_4.getID() );
+        assert ( subscriptionStorePostgres.getSubscriptions( newUser_3.getID() ).isEmpty() );
+        assert ( subscriptionStorePostgres.getFollowers( newUser_4.getID() ).isEmpty() );
+
+        // Users can't subscribe to themselves (to do in handler)
 
     }
 
