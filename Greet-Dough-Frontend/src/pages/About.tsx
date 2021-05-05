4b3b774063a07e3ab96b35f5ddfc21aa120f0de8
Thodori Kapouranis <thodorikap@gmail.com>
2021-05-05 19:47:29 -0400

    Adds simple about page

--- Greet-Dough-Frontend/src/pages/About.tsx
+++ Greet-Dough-Frontend/src/pages/About.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import Header from "../components/globalComponents/Header";
-
+import {
+    Box,
+    Text,
+    Divider,
+    VStack,
+    Center,
+} from "@chakra-ui/react";
 function About(){
     return (
-
         <>
             <Header/>
-            <h1> About page goes here</h1>
+            <Box>
+                <Center>
+                    <VStack>
+                        <Text fontWeight={700} fontSize={"36px"} >
+                            Greet & Dough
+                        </Text>
+
+                        <Text> ECE366 - Software Engineering </Text>
+
+                        <Text> The Cooper Union </Text>
+
+                        <Text paddingBottom={"10px"}> Derek Lee, Thodori Kapouranis, Brian Doan, Steven Lee </Text>
+
+                        <a href={"https://github.com/cooperece366s21/Lee-Ko"}>
+                            <Text fontSize={"24px"} color={"cyan.700"} fontWeight={"500"}> Github URL</Text>
+                        </a>
+
+                    </VStack>
+
+                </Center>
+            </Box>
         </>
 
     );
