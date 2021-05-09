c5f9f8e5c6548b5d77b0fced7e56ca4cff7afa58
doanhub <doan@cooper.edu>
2021-05-09 18:28:49 -0400

    Refactored UserStorePostgresTest

--- Greet-Dough-Backend/src/test/java/store/postgres/UserStorePostgresTest.java
+++ Greet-Dough-Backend/src/test/java/store/postgres/UserStorePostgresTest.java
@@ -1,6 +1,9 @@
 package store.postgres;
 
 import model.User;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import utility.GreetDoughJdbi;
 import utility.ResetDao;
 
@@ -16,17 +19,45 @@ class UserStorePostgresTest extends UserStorePostgres {
 
     private static final Jdbi jdbi = GreetDoughJdbi.create("jdbc:postgresql://localhost:4321/greetdough");
 
+    private static UserStorePostgres userStorePostgres;
+    private static ImageStorePostgres imageStorePostgres;
+
     public UserStorePostgresTest() {
         super(jdbi);
     }
 
-    @Test
-    void test() {
+    @BeforeAll
+    static void setUpAll() {
 
-        UserStorePostgres userStorePostgres = new UserStorePostgres(jdbi);
+        // Delete all the databases (only use the relevant ones)
+        ResetDao.deleteAll(jdbi);
 
-        // Used to DROP and CREATE all tables
+        userStorePostgres = new UserStorePostgres(jdbi);
+        imageStorePostgres = new ImageStorePostgres(jdbi);
+
+    }
+
+    @AfterAll
+    static void tearDownAll() {
         ResetDao.reset(jdbi);
+    }
+
+    @BeforeEach
+    void setUpEach() {
+
+        // Delete the databases
+        imageStorePostgres.delete();
+        userStorePostgres.delete();
+
+
+        // Initialize the databases
+        userStorePostgres.init();
+        imageStorePostgres.init();
+
+    }
+
+    @Test
+    void testAddUser() {
 
         // Test adding and retrieving a user
         User josh = userStorePostgres.addUser("Josh");
@@ -35,21 +66,43 @@ class UserStorePostgresTest extends UserStorePostgres {
         assert ( userAfterWrite.getName().equals("Josh") );
         assert ( userStorePostgres.hasUser( josh.getID() ) );
 
-        // Test searching for a user given the first portion of their name
-        User newUser = userStorePostgres.addUser("Jon");
+    }
+
+    @Test
+    void testSearchUser() {
+
+        // Add users
+        userStorePostgres.addUser("Josh");
+        userStorePostgres.addUser("Jon");
 
         // Prints the names of the users given a list of users
         //      Playing around with mapping a list
         LinkedList<String> correctList = new LinkedList<>();
         correctList.add("Josh"); correctList.add("Jon");
         assert ( userStorePostgres.searchUsers("Jo").stream().map(User::getName).collect(Collectors.toList())
-                    .equals(correctList) );
+                .equals(correctList) );
         assert ( userStorePostgres.searchUsers("jo").stream().map(User::getName).collect(Collectors.toList())
-                    .equals(correctList) );
+                .equals(correctList) );
+
+    }
+
+    @Test
+    void testChangeUser() {
+
+        // Add a user
+        User newUser = userStorePostgres.addUser("Jon");
 
         // Test changing the user's name
         userStorePostgres.changeName( newUser.getID(), "John" );
         assert ( userStorePostgres.getUser(newUser.getID()).getName().equals("John") );
+
+    }
+
+    @Test
+    void testDeleteUser() {
+
+        // Add a user
+        User josh = userStorePostgres.addUser("Josh");
 
         // Test deleting the user
         userStorePostgres.deleteUser( josh.getID() );
